dbfe4f5b8fccd6a780c6c76646acc12fdedaf425
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2022-01-22 13:21:01 -0800

    Increasing dificulty but fully conversant

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,13 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move(Point), 
+    Quit,
+    Write(String),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
 }
 
 struct Point {
@@ -37,6 +40,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Quit => self.quit(),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::ChangeColor(c) => self.change_color(c), 
+            Message::Write(s) => self.echo(s),
+        }
     }
 }
 
--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = [0;101];
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array!\n{:?}", a);
     } else {
-        println!("Meh, I eat arrays like that for breakfast.");
+        println!("Meh, I eat arrays like that for breakfast.\n{:?}", a);
     }
 }
